660c37cdf9f86c5b235c161a9cea252dea17beb1
Ferdia McKeogh <ferdia@mckeogh.tech>
2019-03-19 08:56:19 +0100

    feat: start using smoltcp

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,20 @@
 extern crate panic_semihosting;
 extern crate stm32f1xx_hal as hal;
 
+use core::fmt::Write;
 use cortex_m::asm;
 use cortex_m_semihosting::hprintln;
 use enc28j60::Enc28j60;
-use hal::{pac, prelude::*, spi::Spi};
-use heapless::{consts::*, FnvIndexMap};
-use jnet::{arp, ether, icmp, ipv4, mac, udp, Buffer};
+use hal::{pac, prelude::*, serial::Serial, spi::Spi};
 use rtfm::app;
+use smoltcp::wire::*;
 
-const MAC: mac::Addr = mac::Addr([0x20, 0x18, 0x03, 0x01, 0x00, 0x00]);
-const IP: ipv4::Addr = ipv4::Addr([192, 168, 1, 2]);
+const CPU_HZ: u32 = 64_000_000;
 
 #[app(device = stm32f1xx_hal::pac)]
 const APP: () = {
     static mut LED: hal::gpio::gpioc::PC13<hal::gpio::Output<hal::gpio::PushPull>> = ();
+    static mut SERIAL: hal::serial::Tx<hal::pac::USART1> = ();
     static mut ETH: enc28j60::Enc28j60<
         hal::spi::Spi<
             hal::device::SPI1,
@@ -37,17 +37,52 @@ const APP: () = {
         hprintln!("init").unwrap();
 
         let device: pac::Peripherals = device;
-        let mut core: rtfm::Peripherals = core;
+        let _core: rtfm::Peripherals = core;
 
-        let mut flash = device.FLASH.constrain();
         let mut rcc = device.RCC.constrain();
-        let clocks = rcc.cfgr.freeze(&mut flash.acr);
         let mut afio = device.AFIO.constrain(&mut rcc.apb2);
+
+        // Clocks
+        let clocks = {
+            // Power mode
+            //device.PWR.cr.modify(|_, w| unsafe { w.vos().bits(0x11) });
+            // Flash latency
+            device
+                .FLASH
+                .acr
+                .modify(|_, w| unsafe { w.latency().bits(0x11) });
+
+            let mut flash = device.FLASH.constrain();
+
+            rcc.cfgr
+                .sysclk(CPU_HZ.hz())
+                .pclk1((CPU_HZ / 2).hz())
+                .pclk2(CPU_HZ.hz())
+                .hclk(CPU_HZ.hz())
+                .freeze(&mut flash.acr)
+        };
 
         // GPIO
         let mut gpioa = device.GPIOA.split(&mut rcc.apb2);
-        let _gpiob = device.GPIOB.split(&mut rcc.apb2);
+        let mut gpiob = device.GPIOB.split(&mut rcc.apb2);
         let mut gpioc = device.GPIOC.split(&mut rcc.apb2);
+
+        // Serial
+        let mut serial = {
+            let tx = gpiob.pb6.into_alternate_push_pull(&mut gpiob.crl);
+            let rx = gpiob.pb7;
+            Serial::usart1(
+                device.USART1,
+                (tx, rx),
+                &mut afio.mapr,
+                115_200.bps(),
+                clocks,
+                &mut rcc.apb2,
+            )
+            .split()
+            .0
+        };
+        writeln!(serial, "serial start").unwrap();
 
         // LED
         let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
@@ -55,7 +90,7 @@ const APP: () = {
         led.set_high();
 
         // ENC28J60
-        let mut enc28j60 = {
+        let enc28j60 = {
             // SPI
             let spi = {
                 let sck = gpioa.pa5.into_alternate_push_pull(&mut gpioa.crl);
@@ -93,178 +128,35 @@ const APP: () = {
         };
 
         LED = led;
+        SERIAL = serial;
         ETH = enc28j60;
     }
 
-    #[idle(resources = [LED, ETH])]
+    #[idle(resources = [LED, SERIAL, ETH])]
     fn idle() -> ! {
-        hprintln!("idle").unwrap();
-
-        let mut cache = FnvIndexMap::<_, _, U8>::new();
+        writeln!(resources.SERIAL, "idle").unwrap();
 
         let mut buf = [0; 256];
         loop {
-            let mut buf = Buffer::new(&mut buf);
-            let len = resources.ETH.receive(buf.as_mut()).ok().unwrap();
-            buf.truncate(len);
+            let len = resources.ETH.receive(buf.as_mut()).ok().unwrap() as usize;
+            let new_buf = &buf[..len];
 
-            if let Ok(mut eth) = ether::Frame::parse(buf) {
-                //hprintln!("\nRx({})", eth.as_bytes().len());
-                //hprintln!("* {:?}", eth);
-
-                let src_mac = eth.get_source();
-
-                match eth.get_type() {
-                    ether::Type::Arp => {
-                        if let Ok(arp) = arp::Packet::parse(eth.payload_mut()) {
-                            match arp.downcast() {
-                                Ok(mut arp) => {
-                                    //hprintln!("** {:?}", arp);
-
-                                    if !arp.is_a_probe() {
-                                        cache.insert(arp.get_spa(), arp.get_sha()).ok();
-                                    }
-
-                                    // are they asking for us?
-                                    if arp.get_oper() == arp::Operation::Request
-                                        && arp.get_tpa() == IP
-                                    {
-                                        // reply to the ARP request
-                                        let tha = arp.get_sha();
-                                        let tpa = arp.get_spa();
-
-                                        arp.set_oper(arp::Operation::Reply);
-                                        arp.set_sha(MAC);
-                                        arp.set_spa(IP);
-                                        arp.set_tha(tha);
-                                        arp.set_tpa(tpa);
-                                        //hprintln!("\n** {:?}", arp);
-                                        let arp_len = arp.len();
-
-                                        // update the Ethernet header
-                                        eth.set_destination(tha);
-                                        eth.set_source(MAC);
-                                        eth.truncate(arp_len);
-                                        //hprintln!("* {:?}", eth);
-
-                                        resources.LED.toggle();
-
-                                        //hprintln!("Tx({})", eth.as_bytes().len());
-                                        resources.ETH.transmit(eth.as_bytes()).ok().unwrap();
-                                    }
-                                }
-                                Err(_arp) => {
-                                    // Not a Ethernet/IPv4 ARP packet
-                                    //hprintln!("** {:?}", _arp);
-                                }
-                            }
-                        } else {
-                            // malformed ARP packet
-                            //hprintln!("Err(A)");
-                        }
+            match smoltcp::wire::EthernetFrame::new_checked(&new_buf) {
+                Ok(frame) => match frame.ethertype() {
+                    EthernetProtocol::Arp => {
+                        let arp = ArpPacket::new_checked(frame.payload()).unwrap();
+                        writeln!(resources.SERIAL, "arp: {:?}", arp).unwrap();
                     }
-                    ether::Type::Ipv4 => {
-                        if let Ok(mut ip) = ipv4::Packet::parse(eth.payload_mut()) {
-                            //hprintln!("** {:?}", ip);
-
-                            let src_ip = ip.get_source();
-
-                            if !src_mac.is_broadcast() {
-                                cache.insert(src_ip, src_mac).ok();
-                            }
-
-                            match ip.get_protocol() {
-                                ipv4::Protocol::Icmp => {
-                                    if let Ok(icmp) = icmp::Packet::parse(ip.payload_mut()) {
-                                        match icmp.downcast::<icmp::EchoRequest>() {
-                                            Ok(request) => {
-                                                // is an echo request
-                                                //hprintln!("*** {:?}", request);
-
-                                                let src_mac = cache
-                                                    .get(&src_ip)
-                                                    .unwrap_or_else(|| unimplemented!());
-
-                                                let _reply: icmp::Packet<_, icmp::EchoReply, _> =
-                                                    request.into();
-                                                //hprintln!("\n*** {:?}", _reply);
-
-                                                // update the IP header
-                                                let mut ip = ip.set_source(IP);
-                                                ip.set_destination(src_ip);
-                                                let _ip = ip.update_checksum();
-                                                //hprintln!("** {:?}", _ip);
-
-                                                // update the Ethernet header
-                                                eth.set_destination(*src_mac);
-                                                eth.set_source(MAC);
-                                                //hprintln!("* {:?}", eth);
-
-                                                resources.LED.toggle();
-                                                //hprintln!("Tx({})", eth.as_bytes().len());
-                                                resources
-                                                    .ETH
-                                                    .transmit(eth.as_bytes())
-                                                    .ok()
-                                                    .unwrap();
-                                            }
-                                            Err(_icmp) => {
-                                                //hprintln!("*** {:?}", _icmp);
-                                            }
-                                        }
-                                    } else {
-                                        // Malformed ICMP packet
-                                        //hprintln!("Err(B)");
-                                    }
-                                }
-                                ipv4::Protocol::Udp => {
-                                    if let Ok(mut udp) = udp::Packet::parse(ip.payload_mut()) {
-                                        //hprintln!("*** {:?}", udp);
-
-                                        if let Some(src_mac) = cache.get(&src_ip) {
-                                            let src_port = udp.get_source();
-                                            let dst_port = udp.get_destination();
-
-                                            // update the UDP header
-                                            udp.set_source(dst_port);
-                                            udp.set_destination(src_port);
-                                            udp.zero_checksum();
-                                            //hprintln!("\n*** {:?}", udp);
-
-                                            // update the IP header
-                                            let mut ip = ip.set_source(IP);
-                                            ip.set_destination(src_ip);
-                                            let ip = ip.update_checksum();
-                                            let ip_len = ip.len();
-                                            //hprintln!("** {:?}", ip);
-
-                                            // update the Ethernet header
-                                            eth.set_destination(*src_mac);
-                                            eth.set_source(MAC);
-                                            eth.truncate(ip_len);
-                                            //hprintln!("* {:?}", eth);
-
-                                            resources.LED.toggle();
-                                            //hprintln!("Tx({})", eth.as_bytes().len());
-                                            resources.ETH.transmit(eth.as_bytes()).ok().unwrap();
-                                        }
-                                    } else {
-                                        // malformed UDP packet
-                                        //hprintln!("Err(C)");
-                                    }
-                                }
-                                _ => {}
-                            }
-                        } else {
-                            // malformed IPv4 packet
-                            //hprintln!("Err(D)");
-                        }
+                    EthernetProtocol::Ipv4 => {
+                        writeln!(resources.SERIAL, "ipv4").unwrap();
                     }
-                    _ => {}
+                    _ => {
+                        unimplemented!();
+                    }
+                },
+                Err(e) => {
+                    writeln!(resources.SERIAL, "malformed Ethernet frame, {:?}", e).unwrap();
                 }
-            } else {
-                // malformed Ethernet frame
-                //hprintln!("Err(E)");
             }
         }
     }
@@ -274,6 +166,6 @@ struct AsmDelay {}
 
 impl embedded_hal::blocking::delay::DelayMs<u8> for AsmDelay {
     fn delay_ms(&mut self, ms: u8) {
-        asm::delay((ms as u32) * (8_000_000 / 10));
+        asm::delay((ms as u32) * (CPU_HZ / 10));
     }
 }
